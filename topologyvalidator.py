b703eb2d621d54800108449c882fe12a4f9d11aa
enriquez <enrico.russo@gmail.com>
2018-03-14 21:52:44 +0100

    multiple requirements support

--- topologyvalidator.py
+++ topologyvalidator.py
@@ -8,7 +8,7 @@ class TopologyValidator():
 
 	# Checks whether type1 is coherent with type2 (i.e., whether type1 is the same type or a sub-type of type2)
 	def checkTypeCoherence(self, type1, type2):
-		
+
 		passed = (type1 == type2)
 
 		if self.customDefinition.has_key(type1):
@@ -34,10 +34,11 @@ class TopologyValidator():
 
 		# check whether "nodeType" defines "reqName"
 		reqDef = None
-		if nodeTypeDef.has_key("requirements"): 
-			reqDefinitions = nodeTypeDef.get('requirements')[0]
-			if reqName in reqDefinitions:
-				reqDef = reqDefinitions.get(reqName)
+		if nodeTypeDef.has_key("requirements"):
+			# reqDefinitions = nodeTypeDef.get('requirements')[0]
+			for reqDefinitions in nodeTypeDef.get('requirements'):
+				if reqName in reqDefinitions:
+					reqDef = reqDefinitions.get(reqName)
 
 		# if "reqName" has been found, returns the corresponding "reqDef"
 		if reqDef is not None:
@@ -54,7 +55,7 @@ class TopologyValidator():
 
 		# check whether "nodeType" defines "capName"
 		capDef = None
-		if nodeTypeDef.has_key("capabilities"): 
+		if nodeTypeDef.has_key("capabilities"):
 			capDefinitions = nodeTypeDef.get('capabilities')
 			if capName in capDefinitions:
 				capDef = capDefinitions.get(capName)
@@ -62,7 +63,7 @@ class TopologyValidator():
 		# if "capName" has not been found, looks for "capName" in the parent "nodeType" (if any)
 		if capDef is None and nodeTypeDef.has_key('derived_from'):
 			return self.getCapabilityDefinition(capName,nodeTypeDef.get('derived_from'))
-		
+
 		return capDef
 
 	# Returns the definition of the node with type "nodeType" (if any)
@@ -73,19 +74,19 @@ class TopologyValidator():
 			typeDef = self.customDefinition.get(nodeType)
 		elif self.toscaBaseTypes.has_key(nodeType):
 		    typeDef = self.toscaBaseTypes.get(nodeType)
-		
+
 		return typeDef
 
 	# Returns the node template of the node called "nodeName" (if any)
 	def getNode(self, nodeName, nodeTemplates):
-		# Assigns the type of "nodeTemplate" to "nodeType" 
+		# Assigns the type of "nodeTemplate" to "nodeType"
 		node = None
-		for n in nodeTemplates: 
+		for n in nodeTemplates:
 			if n.name == nodeName:
 				node = n
 		return node
 
-	# Check whether the "node_type_name" of the requirement definition "reqDef" is coherent with "nodeType" 
+	# Check whether the "node_type_name" of the requirement definition "reqDef" is coherent with "nodeType"
 	def checkNodeType(self, reqDef, nodeType):
 		if reqDef.has_key('node'):
 			reqDefNodeType = reqDef.get('node')
@@ -102,11 +103,11 @@ class TopologyValidator():
 			if self.getCapabilityDefinition(capability, node.type) is not None:
 				if self.getCapabilityDefinition(capability, node.type).has_key('type'):
 					capabilityType = self.getCapabilityDefinition(capability, node.type).get('type')
-		return capabilityType 
+		return capabilityType
 
-	# Check whether the the type of the capability "capability" is coherent with the type of the capability defined from the requirement definition "reqDef" 
+	# Check whether the the type of the capability "capability" is coherent with the type of the capability defined from the requirement definition "reqDef"
 	def checkCapabilityType(self, reqDef, capability, node):
-		
+
 		# Assigns the capability type of the requirement definition to "reqDefCap"
 		if reqDef.has_key('capability'):
 			reqDefCap = reqDef.get('capability')
@@ -115,7 +116,7 @@ class TopologyValidator():
 			return self.checkTypeCoherence(capabilityType, reqDefCap)
 		return True
 
-	# Check whether the type of the capabilities of "capabilityList" are coherent with the type of the capability defined from the requirement definition "reqDef" 
+	# Check whether the type of the capabilities of "capabilityList" are coherent with the type of the capability defined from the requirement definition "reqDef"
 	def checkCapabilitiesType(self, reqDef, capabilityList, node):
 
 		for capability in capabilityList:
@@ -135,12 +136,12 @@ class TopologyValidator():
 			else:
 				for rt in node.get_relationship_template():
 					if rt.name == relationship:
-						relationshipType = rt.type	
+						relationshipType = rt.type
 		return relationshipType
 
 	# Check whether the type of the relationship "relationship" is coherent with the type defined from the relationship definition "reqDef"
 	def checkRelationshipType(self, reqDef, relationship, node):
-		
+
 		# Assigns the relationship type of the requirement definition to "reqDefRel"
 		if reqDef.has_key('relationship'):
 			reqDefRel = reqDef.get('relationship')
@@ -156,7 +157,7 @@ class TopologyValidator():
 
 	# Check whether the relationship is valid and return the type of the target capability or None
 	def checkRelationship(self, reqDef, relationship, targetNode, node, targetCapability):
-		
+
 		# Assigns the relationship type of the requirement definition to "reqDefRel"
 		if reqDef.has_key('relationship'):
 			reqDefRel = reqDef.get('relationship')
@@ -181,7 +182,7 @@ class TopologyValidator():
 						if self.checkTypeCoherence(targetCapabilityType,validTargetType) == True:
 							return targetCapabilityType
 					return None
-				# Case 2 - the relationship_template of the target is a node  
+				# Case 2 - the relationship_template of the target is a node
 				else:
 					# Retrieves the capabilities of the target node
 					targetCapabilities = targetNode.get_capabilities()
@@ -196,10 +197,10 @@ class TopologyValidator():
 	# Returns the field "valid_target_types" of the relationship with type "relationshipType"
 	def getValidTargetTypes(self, relationshipType):
 
-		relationshipTypeDef = self.getTypeDefinition(relationshipType)  
-		
+		relationshipTypeDef = self.getTypeDefinition(relationshipType)
+
 		validTargetTypes = None
-		if relationshipTypeDef.has_key("valid_target_types"): 
+		if relationshipTypeDef.has_key("valid_target_types"):
 				validTargetTypes = relationshipTypeDef.get('valid_target_types')
 
 		# if "validTargetTypes" has been found, returns the "valid_target_types"
@@ -207,17 +208,17 @@ class TopologyValidator():
 			return validTargetTypes
 		# otherwise, looks for "validTargetTypes" in the parent "relationshipType" (if any)
 		if relationshipTypeDef.has_key("derived_from"):
-			return self.getValidTargetTypes(relationshipTypeDef.get('derived_from')) 
-		# if there is no parent "relationship", returns "None" 
+			return self.getValidTargetTypes(relationshipTypeDef.get('derived_from'))
+		# if there is no parent "relationship", returns "None"
 		return None
 
 	# Returns the field "valid_source_types" of a node with type "nodeType"
 	def getValidSourceTypesNode(self, capabilityType, nodeType):
 
-		nodeTypeDef = self.getTypeDefinition(nodeType)  
-		
+		nodeTypeDef = self.getTypeDefinition(nodeType)
+
 		validSourceTypes = None
-		if nodeTypeDef.has_key("capabilities"): 
+		if nodeTypeDef.has_key("capabilities"):
 			capabilities = nodeTypeDef.get('capabilities')
 			for cap in capabilities.values():
 				if cap.get('type') == capabilityType:
@@ -228,23 +229,23 @@ class TopologyValidator():
 			return validSourceTypes
 		# otherwise, looks for "validTargetTypes" in the parent "nodeType" (if any)
 		if nodeTypeDef.has_key("derived_from"):
-			return self.getValidSourceTypesNode(capabilityType,nodeTypeDef.get('derived_from')) 
-		# if there is no parent "nodeType", returns "None" 
+			return self.getValidSourceTypesNode(capabilityType,nodeTypeDef.get('derived_from'))
+		# if there is no parent "nodeType", returns "None"
 		return None
 
 	# Check whether the types of the nodes defined from the field "valid_source_types" of a "target node" are coherent with the type of a "source node"
 	def checkValidSourceTypesNode(self, targetCapabilityType, targetNode, node):
 
 		validSourceTypes = self.getValidSourceTypesNode(targetCapabilityType,targetNode.type)
-		
+
 		if validSourceTypes is not None:
 			for elem in validSourceTypes:
 				if self.checkTypeCoherence(node.type,elem) is True:
-					return True  
+					return True
 			return False
 		return True
-		
-	# Returns the field "valid_source_types" of a capability with type "capabilityType" 
+
+	# Returns the field "valid_source_types" of a capability with type "capabilityType"
 	def getValidSourceTypesCapability(self, capabilityType):
 
 		capTypeDef = self.getTypeDefinition(capabilityType)
@@ -258,15 +259,15 @@ class TopologyValidator():
 			return validSourceTypes
 		# otherwise, looks for "validSourceTypes" in the parent "capType"
 		if capTypeDef.has_key("derived_from"):
-			return self.getValidSourceTypesCapability(capTypeDef.get('derived_from')) 	
-		# if there is no parent "nodeType", returns "None" 
-		return None 
+			return self.getValidSourceTypesCapability(capTypeDef.get('derived_from'))
+		# if there is no parent "nodeType", returns "None"
+		return None
 
 	# Check whether the types of the nodes defined from the field "valid_source_types" of a capability are coherent with the type of a "source node"
 	def checkValidSourceTypesCapability(self, capabilityType, node):
-		
+
 		validSourceTypes = self.getValidSourceTypesCapability(capabilityType)
-		
+
 		if validSourceTypes is not None:
 			for elem in validSourceTypes:
 				if self.checkTypeCoherence(node.type,elem) == True:
@@ -295,7 +296,7 @@ class TopologyValidator():
 
 	                validation[node.name] = {}
 
-	                for r in reqs: 
+	                for r in reqs:
 	                    rName = r.keys()[0]
 	                    reqError = []
 
@@ -323,7 +324,7 @@ class TopologyValidator():
 	                            if "relationship" in rProps:
 	                                relationship = rProps.get("relationship")
 
-	                        # Retrieves the dictionary defining the target node 
+	                        # Retrieves the dictionary defining the target node
 	                        targetNode = self.getNode(targetNode,nodetemplates)
 
 	                        # Check if there is a target node
@@ -339,7 +340,7 @@ class TopologyValidator():
 	                        # Assigns the capability type of the target node to "targetCapabilityType"
 	                        if targetCapability is not None:
 	                            # Checks whether the target node capability is coherent with that declared in "reqDef"
-	                            if self.checkCapabilityType(reqDef, targetCapability, targetNode) is False:  
+	                            if self.checkCapabilityType(reqDef, targetCapability, targetNode) is False:
 	                                reqError.append([1.3])
 
 	                        else:
@@ -353,7 +354,7 @@ class TopologyValidator():
 	                        # Assigns the relationship type of the target node to "relationshipType"
 	                        if relationship is not None:
 
-	                        	# Check whether the type of the relationship is coherent with that declared in "reqDef" 
+	                        	# Check whether the type of the relationship is coherent with that declared in "reqDef"
 	                            if self.checkRelationshipType(reqDef,relationship,node) == False:
 	                                reqError.append([1.5, targetNode.name])
 
@@ -361,8 +362,8 @@ class TopologyValidator():
 	                            targetCapabilityType = self.checkRelationship(reqDef,relationship,targetNode,node,targetCapability)
 	                            # Case 1 - the "targetCapabilityType" is not valid
 	                            if targetCapabilityType is None:
-	                            	# Case 1.1 - the targetCapability is defined, but its type is not valid with that required 
-	                            	if targetCapability is not None: 
+	                            	# Case 1.1 - the targetCapability is defined, but its type is not valid with that required
+	                            	if targetCapability is not None:
 	                            		reqError.append([2.1, targetCapability])
 	                            	# Case 1.2 - the targetCapability is not defined and the target node does not offer valid capabilities
 	                            	else:
@@ -377,6 +378,6 @@ class TopologyValidator():
 	                            		reqError.append([3.2, node.type, targetNode.name])
 
 	                    validation[node.name].update({rName: reqError})
-	                    
-	                        
+
+
 	    return validation
